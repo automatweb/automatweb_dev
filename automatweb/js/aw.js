f011d1d9cbd8d4d14cdba22df11fdfc10adad571
kristo <kristo>
2001-06-28 18:03:55 +0000

    *** empty log message ***

--- automatweb/js/aw.js
+++ automatweb/js/aw.js
@@ -24,7 +24,7 @@ function aw_popup(file,name,width,height)
 
 function aw_popup_s(file,name,width,height)
 {
-	_aw_popup(file,name,0,1,0,0,1,1,width,height);
+	_aw_popup(file,name,0,0,0,0,0,0,width,height);
 };
 
 function aw_popup_scroll(file,name,width,height)
