ced590cef076ddb531669baee6f248b6ea8ac00b
duke <duke>
2002-04-10 01:03:21 +0000

    + directory for the site and a basic apache configuration file are created + basic directory structure for the site is created

--- scripts/install/install.c
+++ scripts/install/install.c
@@ -1,27 +1,36 @@
 /*
  * AW install script
  * path to configuration file should probably come from command line
- * $Header: /home/cvs/automatweb_dev/scripts/install/Attic/install.c,v 1.2 2002/04/04 22:40:59 duke Exp $
+ * $Header: /home/cvs/automatweb_dev/scripts/install/Attic/install.c,v 1.3 2002/04/10 01:03:21 duke Exp $
  */
 #include <stdio.h>
 #include <string.h>
+#include <errno.h>
+// for mkdir
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+
 main(int argc, char **argv)
 {
 	FILE *fpconfig; // config file 
 	FILE *pmysql; // pipe to mysql
 
 	// these come from stdin/client
-	char dbname[60], dbhost[60], dbuser[60], dbpass[60];
+	char dbname[60], dbhost[60], dbuser[60], dbpass[60], sitename[60];
 
 	// these come from install.ini
 	char mysql_host[30], mysql_user[30], mysql_pass[30], mysql_client[30];
-	char default_user[30], default_pass[30];
+	char default_user[30], default_pass[30], wwwroot[30];
 
 	// for internal use
-	char cline[100],myline[100],cmdline[200];
+	char cline[100],myline[100],cmdline[200],path[100];
 	char *ckey,*cval;
 
-	if ((fpconfig = fopen("install.ini","r")) == NULL)
+	extern int errno;
+
+	if ((fpconfig = fopen("/www/automatweb_dev/scripts/install/install.ini","r")) == NULL)
 	{
 		printf("Can't open configuration file!\n");
 		return 1;
@@ -51,15 +60,29 @@ main(int argc, char **argv)
 			else if ( strcmp(ckey,"mysql.client") == 0)
 			{
 				strncpy(mysql_client,cval,sizeof(mysql_client)-1);
+			}
+			else if ( strcmp(ckey,"default_user") == 0)
+			{
+				strncpy(default_user,cval,sizeof(default_user)-1);
+			}
+			else if ( strcmp(ckey,"default_pass") == 0)
+			{
+				strncpy(default_pass,cval,sizeof(default_pass)-1);
+			}
+			else if ( strcmp(ckey,"wwwroot") == 0)
+			{
+				strncpy(wwwroot,cval,sizeof(wwwroot)-1);
 			};
 		};
 		fclose(fpconfig);
 	};
 
-
 	// get the information from the client
+	// tegelikult võiks see vajalik kataloogistrukuur tulla ju tar failist?
+	// oh yes.
 	printf("AW installer\n");
-	printf("db.name: ");
+	
+	printf("db.name (will also be used as sitename: ");
 	scanf("%20s",dbname);
 	printf("db.host: ");
 	scanf("%20s",dbhost);
@@ -67,10 +90,31 @@ main(int argc, char **argv)
 	scanf("%20s",dbuser);
 	printf("db.pass: ");
 	scanf("%20s",dbpass);
-	printf("default.user: ");
+	printf("default.user (%s): ",default_user);
 	scanf("%20s",default_user);
-	printf("default.pass: ");
+	printf("default.pass (%s): ",default_pass);
 	scanf("%20s",default_pass);
+
+	strncpy(sitename,dbname,sizeof(sitename)-1);
+	
+	strncpy(path,wwwroot,sizeof(wwwroot)-1);
+
+	strcat(path,sitename);
+	create_dir(path);
+	copy_site_files(path);
+	create_apache_conf(path,sitename);
+
+	return 0;
+
+	// create required directories
+	// site_dir
+	// pagecache
+	// public
+	// public/img
+	// archive
+	// files
+	//
+	//
 
 	// get the database dump
 	sprintf(cmdline,"mysqldump -d -h %s -u %s --password=%s samaw > /tmp/dump.tmp",mysql_host,mysql_user,mysql_pass);
@@ -104,13 +148,13 @@ main(int argc, char **argv)
 	unlink("/tmp/dump.tmp");
 	printf("Import done\n");
 
+	/*
 	printf("Creating default entries\n");
 	sprintf(cmdline,"mysql -h %s -u %s --password=%s %s",mysql_host,dbuser,dbpass,dbname);
 	printf("executing %s\n",cmdline);
 	if ((pmysql = popen(cmdline,"w")) == NULL)
 	{
 		printf("Can't open pipe to MySQL!\n");
-		// drop dead
 		return 1;
 	};
 
@@ -147,6 +191,7 @@ main(int argc, char **argv)
 	fputs(myline,pmysql);
 	
 	pclose(pmysql);
+	*/
 
 	printf("NICE ONE, BROTHA!!!!\n");
 	return 0;
