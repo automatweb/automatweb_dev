783d7f74f70bfbe0ab1cf8289f16868f94d016dc
kristo <kristo>
2003-02-15 19:14:22 +0000

    + removed old installer su-exec scripts

--- scripts/install/support.c
+++
@@ -1,9 +0,0 @@
-/*
- * support functions
- * $Header: /home/cvs/automatweb_dev/scripts/install/Attic/support.c,v 1.2 2002/06/10 15:51:01 kristo Exp $
- */
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-
