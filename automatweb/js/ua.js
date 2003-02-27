d112c106a97c1cbcc4721aa4d16974051ab9c6f8
duke <duke>
2003-02-27 16:14:32 +0000

    + new treeview version

--- automatweb/js/ua.js
+++ automatweb/js/ua.js
@@ -1,164 +1,143 @@
-// ua.js - Detect Browser
-// Requires JavaScript 1.1
 /*
-The contents of this file are subject to the Netscape Public
-License Version 1.1 (the "License"); you may not use this file
-except in compliance with the License. You may obtain a copy of
-the License at http://www.mozilla.org/NPL/
+ * $Log: ua.js,v $
+ * Revision 2.2  2003/02/27 16:14:32  duke
+ * + new treeview version
+ *
+ * Revision 1.9  2002/07/22 14:06:21  bc6ix
+ * fix license path, change version reporting to use 2 digits for each level
+ *
+ * Revision 1.8  2002/07/07 08:23:07  bc6ix
+ * fix line endings
+ *
+ * Revision 1.7  2002/05/14 16:52:52  bc6ix
+ * use CVS Log for revision history
+ *
+ *
+ */
 
-Software distributed under the License is distributed on an "AS
-IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
-implied. See the License for the specific language governing
-rights and limitations under the License.
+/* ***** BEGIN LICENSE BLOCK *****
+ * Licensed under Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ * Full Terms at http://bclary.com/lib/js/license/mpl-tri-license.txt
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Netscape code.
+ *
+ * The Initial Developer of the Original Code is
+ * Netscape Corporation.
+ * Portions created by the Initial Developer are Copyright (C) 2001
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s): Bob Clary <bclary@netscape.com>
+ *
+ * ***** END LICENSE BLOCK ***** */
 
-The Initial Developer of the Original Code is Bob Clary.
-
-Contributor(s): Bob Clary, Original Work, Copyright 1999-2000
-                Bob Clary, Netscape Communications, Copyright 2001
-
-Alternatively, the contents of this file may be used under the
-terms of the GNU Public License (the "GPL"), in which case the
-provisions of the GPL are applicable instead of those above.
-If you wish to allow use of your version of this file only
-under the terms of the GPL and not to allow others to use your
-version of this file under the NPL, indicate your decision by
-deleting the provisions above and replace them with the notice
-and other provisions required by the GPL.  If you do not delete
-the provisions above, a recipient may use your version of this
-file under either the NPL or the GPL.
-*/
-
-// work around bug in xpcdom Mozilla 0.9.1
-window.saveNavigator = window.navigator;
-
-// Handy functions
-function noop() {}
-function noerror() { return true; }
-
-function defaultOnError(msg, url, line)
+function xbDetectBrowser()
 {
-	// customize this for your site
-	if (top.location.href.indexOf('_files/errors/') == -1)
-		top.location = '/evangelism/xbProjects/_files/errors/index.html?msg=' + escape(msg) + '&url=' + escape(url) + '&line=' + escape(line);
-}
+  var oldOnError = window.onerror;
+  var element = null;
 
-// Display Error page... 
-// XXX: more work to be done here
-//
-function reportError(message)
-{
-	// customize this for your site
-	if (top.location.href.indexOf('_files/errors/') == -1)
-		top.location = '/evangelism/xbProjects/_files/errors/index.html?msg=' + escape(message);
-}
+  window.onerror = null;
+  
+  // work around bug in xpcdom Mozilla 0.9.1
+  window.saveNavigator = window.navigator;
 
-function pageRequires(cond, msg, redirectTo)
-{
-	if (!cond)
-	{
-		msg = 'This page requires ' + msg;
-		top.location = redirectTo + '?msg=' + escape(msg);
-	}
-	// return cond so can use in <A> onclick handlers to exclude browsers
-	// from pages they do not support.
-	return cond;
-}
+  navigator.OS    = '';
+  navigator.version  = parseFloat(navigator.appVersion);
+  navigator.org    = '';
+  navigator.family  = '';
 
-function detectBrowser()
-{
-	var oldOnError = window.onerror;
-	var element = null;
-	
-	window.onerror = defaultOnError;
+  var platform;
+  if (typeof(window.navigator.platform) != 'undefined')
+  {
+    platform = window.navigator.platform.toLowerCase();
+    if (platform.indexOf('win') != -1)
+      navigator.OS = 'win';
+    else if (platform.indexOf('mac') != -1)
+      navigator.OS = 'mac';
+    else if (platform.indexOf('unix') != -1 || platform.indexOf('linux') != -1 || platform.indexOf('sun') != -1)
+      navigator.OS = 'nix';
+  }
 
-	navigator.OS		= '';
-	navigator.version	= 0;
-	navigator.org		= '';
-	navigator.family	= '';
-
-	var platform;
-	if (typeof(window.navigator.platform) != 'undefined')
-	{
-		platform = window.navigator.platform.toLowerCase();
-		if (platform.indexOf('win') != -1)
-			navigator.OS = 'win';
-		else if (platform.indexOf('mac') != -1)
-			navigator.OS = 'mac';
-		else if (platform.indexOf('unix') != -1 || platform.indexOf('linux') != -1 || platform.indexOf('sun') != -1)
-			navigator.OS = 'nix';
-	}
-
-	var i = 0;
-	var ua = window.navigator.userAgent.toLowerCase();
-	
-	if (ua.indexOf('opera') != -1)
-	{
-		i = ua.indexOf('opera');
-		navigator.family	= 'opera';
-		navigator.org		= 'opera';
-		navigator.version	= parseFloat('0' + ua.substr(i+6), 10);
-	}
-	else if ((i = ua.indexOf('msie')) != -1)
-	{
-		navigator.org		= 'microsoft';
-		navigator.version	= parseFloat('0' + ua.substr(i+5), 10);
-		
-		if (navigator.version < 4)
-			navigator.family = 'ie3';
-		else
-			navigator.family = 'ie4'
-	}
-	else if (typeof(window.controllers) != 'undefined' && typeof(window.locationbar) != 'undefined')
-	{
-		i = ua.lastIndexOf('/')
-		navigator.version = parseFloat('0' + ua.substr(i+1), 10);
-		navigator.family = 'gecko';
-
-		if (ua.indexOf('netscape') != -1)
-			navigator.org = 'netscape';
-		else if (ua.indexOf('compuserve') != -1)
-			navigator.org = 'compuserve';
-		else
-			navigator.org = 'mozilla';
-	}
-	else if ((ua.indexOf('mozilla') !=-1) && (ua.indexOf('spoofer')==-1) && (ua.indexOf('compatible') == -1) && (ua.indexOf('opera')==-1)&& (ua.indexOf('webtv')==-1) && (ua.indexOf('hotjava')==-1))
-	{
-	    var is_major = parseFloat(navigator.appVersion);
+  var i = 0;
+  var ua = window.navigator.userAgent.toLowerCase();
+  
+  if (ua.indexOf('opera') != -1)
+  {
+    i = ua.indexOf('opera');
+    navigator.family  = 'opera';
+    navigator.org    = 'opera';
+    navigator.version  = parseFloat('0' + ua.substr(i+6), 10);
+  }
+  else if ((i = ua.indexOf('msie')) != -1)
+  {
+    navigator.org    = 'microsoft';
+    navigator.version  = parseFloat('0' + ua.substr(i+5), 10);
     
-		if (is_major < 4)
-			navigator.version = is_major;
-		else
-		{
-			i = ua.lastIndexOf('/')
-			navigator.version = parseFloat('0' + ua.substr(i+1), 10);
-		}
-		navigator.org = 'netscape';
-		navigator.family = 'nn' + parseInt(navigator.appVersion);
-	}
-	else if ((i = ua.indexOf('aol')) != -1 )
-	{
-		// aol
-		navigator.family	= 'aol';
-		navigator.org		= 'aol';
-		navigator.version	= parseFloat('0' + ua.substr(i+4), 10);
-	}
+    if (navigator.version < 4)
+      navigator.family = 'ie3';
+    else
+      navigator.family = 'ie4'
+  }
+  else if (ua.indexOf('gecko') != -1)
+  {
+    navigator.family = 'gecko';
+    var rvStart = ua.indexOf('rv:');
+    var rvEnd   = ua.indexOf(')', rvStart);
+    var rv      = ua.substring(rvStart+3, rvEnd);
+    var rvParts = rv.split('.');
+    var rvValue = 0;
+    var exp     = 1;
 
-	navigator.DOMCORE1	= (typeof(document.getElementsByTagName) != 'undefined' && typeof(document.createElement) != 'undefined');
-	navigator.DOMCORE2	= (navigator.DOMCORE1 && typeof(document.getElementById) != 'undefined' && typeof(document.createElementNS) != 'undefined');
-	navigator.DOMHTML	= (navigator.DOMCORE1 && typeof(document.getElementById) != 'undefined');
-	navigator.DOMCSS1	= ( (navigator.family == 'gecko') || (navigator.family == 'ie4') );
+    for (var i = 0; i < rvParts.length; i++)
+    {
+      var val = parseInt(rvParts[i]);
+      rvValue += val / exp;
+      exp *= 100;
+    }
+    navigator.version = rvValue;
 
-	navigator.DOMCSS2   = false;
-	if (navigator.DOMCORE1)
-	{
-		element = document.createElement('p');
-		navigator.DOMCSS2 = (typeof(element.style) == 'object');
-	}
+    if (ua.indexOf('netscape') != -1)
+      navigator.org = 'netscape';
+    else if (ua.indexOf('compuserve') != -1)
+      navigator.org = 'compuserve';
+    else
+      navigator.org = 'mozilla';
+  }
+  else if ((ua.indexOf('mozilla') !=-1) && (ua.indexOf('spoofer')==-1) && (ua.indexOf('compatible') == -1) && (ua.indexOf('opera')==-1)&& (ua.indexOf('webtv')==-1) && (ua.indexOf('hotjava')==-1))
+  {
+    var is_major = parseFloat(navigator.appVersion);
+    
+    if (is_major < 4)
+      navigator.version = is_major;
+    else
+    {
+      i = ua.lastIndexOf('/')
+      navigator.version = parseFloat('0' + ua.substr(i+1), 10);
+    }
+    navigator.org = 'netscape';
+    navigator.family = 'nn' + parseInt(navigator.appVersion);
+  }
+  else if ((i = ua.indexOf('aol')) != -1 )
+  {
+    // aol
+    navigator.family  = 'aol';
+    navigator.org    = 'aol';
+    navigator.version  = parseFloat('0' + ua.substr(i+4), 10);
+  }
+  else if ((i = ua.indexOf('hotjava')) != -1 )
+  {
+    // hotjava
+    navigator.family  = 'hotjava';
+    navigator.org    = 'sun';
+    navigator.version  = parseFloat(navigator.appVersion);
+  }
 
-	navigator.DOMEVENTS	= (typeof(document.createEvent) != 'undefined');
-
-	window.onerror = oldOnError;
+  window.onerror = oldOnError;
 }
 
-detectBrowser();
+xbDetectBrowser();
 
