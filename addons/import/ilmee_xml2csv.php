6647a1477b2bfabe61e2c388f92c08ef5a6d06f2
duke <duke>
2003-05-02 15:07:57 +0000

    well then...

--- addons/import/ilmee_xml2csv.php
+++ addons/import/ilmee_xml2csv.php
@@ -1,38 +1,54 @@
 <?php
-// Retrieves a list of temperatures for different cities in Estonia
-// and outputs a csv file of the data we are interested in, that
-// can then be loaded into a MySQL database with LOAD DATA infile
-/* sample XML snippet
-<blokk id="R">
-	<linn1>Jõgeva</linn1><temp1>3.8</temp1><linn2>Pärnu</linn2><temp2>2</temp2><linn3>Rapla</linn3><temp3>2.4/temp3>
-</blokk>
-*/
+// Retrieves weather forecast information for next 4 days
 // sample crontab line:
-// * */4 * * * /www/automatweb_dev/scripts/php -q /www/automatweb_dev/addons/import/ilmee_xml2csv.php > /tmp/ilmee.csv && /usr/ local/bin/mysqlimport -h hell -r --fields-terminated-by=';' -v -u curimport --password='XXX' automatweb /tmp/ilmee.csv
-
-//
+// * */4 * * * /www/automatweb_dev/scripts/php -q /www/automatweb_dev/addons/import/ilmee_xml2csv.php > /tmp/ilmee.txt 
 $table = "ilmee_data";
 $file = join("",file("http://www.ilm.ee/~data/include/ilm_xml.php3"));
 $p = xml_parser_create();
 xml_parse_into_struct($p,$file,$vals,$index);
 xml_parser_free($p);
 $tmps = array();
+$ids = array();
 foreach($vals as $key => $val)
 {
-	if (preg_match("/^LINN(\d*)$/",$val["tag"],$m))
+	if ( ($val["tag"] == "RUBRIIK") && ($val["attributes"]["NIMI"] == "EMHI") )
 	{
-		$tmps[$m[1]]["linn"] = $val["value"];
+		$open = true;
 	};
-	if (preg_match("/^TEMP(\d*)$/",$val["tag"],$m))
+	if ( ($val["tag"] == "BLOKK") && ($val["type"] == "open"))
 	{
-		$tmps[$m[1]]["temp"] = $val["value"];
+		$id = $val["attributes"]["ID"];
 	};
-};
-if (sizeof($tmps) > 0)
-{
-	foreach($tmps as $block)
+
+	if ($val["tag"] == "DATE")
 	{
-		printf("%s;%s\n",$block["linn"],$block["temp"]);
+		$date = $val["value"];
 	};
+
+	if ($val["tag"] == "OO")
+	{
+		$oo = $val["value"];
+	};
+
+	if ($val["tag"] == "PAEV")
+	{
+		$paev = $val["value"];
+	};
+
+	if ($val["tag"] == "NAHTUS")
+	{
+		$nahtus = $val["value"];
+	};
+
+	if ( ($val["tag"] == "BLOKK") && ($val["type"] == "close") && $open)
+	{
+		print "$id|$date|$oo|$paev|$nahtus\n";
+	};
+
+	if ( ($val["tag"] == "RUBRIIK") && ($val["type"] == "close") )
+	{
+		$open = false;
+	};
+		
 }
 ?>
