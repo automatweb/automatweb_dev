59581085fdda8d869c0159b4f0aeaf1060957c1a
voldemar <voldemar>
2006-01-05 11:17:15 +0000

    debug display uri query control added

--- automatweb/js/autosuggest.js
+++ automatweb/js/autosuggest.js
@@ -123,6 +123,39 @@ function getHttp()
 	return http;
 }
 
+function getParameter ( parameterName )
+{
+	// Add "=" to the parameter name (i.e. parameterName=value)
+	var parameterName = parameterName + "=";
+	var queryString = window.top.location.search;
+
+	if ( queryString.length > 0 )
+	{
+		// Find the beginning of the string
+		begin = queryString.indexOf ( parameterName );
+
+		// If the parameter name is not found, skip it, otherwise return the value
+		if ( begin != -1 )
+		{
+			// Add the length (integer) to the beginning
+			begin += parameterName.length;
+			// Multiple parameters are separated by the "&" sign
+			end = queryString.indexOf ( "&" , begin );
+
+			if ( end == -1 )
+			{
+				end = queryString.length
+			}
+
+			// Return the string
+			return unescape ( queryString.substring ( begin, end ) );
+		}
+
+		// Return "null" if no parameter has been found
+		return "null";
+	}
+}
+
 function getAutoCompleteOptions (elem, suggestionsUrl, params, isTuple)
 {
 	suggestions = new Array();
@@ -160,9 +193,7 @@ function getAutoCompleteOptions (elem, suggestionsUrl, params, isTuple)
 
 		suggestionsUrl = suggestionsUrl + requestSeparator + requestParams + selfParam;
 
-//~ dbgdiv = document.getElementById("help_layer");
-//~ dbgdiv.style.display = "block";
-//~ dbgdiv.innerHTML = suggestionsUrl;
+/* dbg */ if (getParameter ("ACdbg") == 1) { dbgdiv = document.getElementById("help_layer"); dbgdiv.style.display = "block"; dbgdiv.innerHTML = suggestionsUrl; }
 
 		httpRequest.open("GET", suggestionsUrl, true);
 		//~ httpRequest.setRequestHeader("Content-Type", "application/x-javascript; charset=ISO-8859-15");
