6ea0b61cd1672abf540c244509a1c41223948adc
hannes <hannes>
2008-10-23 11:28:06 +0000

    + can add new params + doesnt forget prior changes in current script
    Bug  #237194
    Time:

--- automatweb/js/jquery/plugins/jquery_sup.js
+++ automatweb/js/jquery/plugins/jquery_sup.js
@@ -8,19 +8,30 @@
             return new $.sup(param, value);
         },
         sup: function(param, value){
+			if (typeof(url) == "undefined")
+			{
+				url = document.location+"";
+			}
 			param = param.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
 			var regexS = "([\\?&]"+param+"=)([^&#]*)";
 			var regex = new RegExp( regexS );
 			var results = regex.exec( window.location.href );
 			if( results == null )
 			{
-		    	return "";
+				if ( url.indexOf( "?", url ) > 0)
+				{
+					url += "&"+param+"="+value;
+				}
+				else
+				{
+					url += "?"+param+"="+value;
+				}
 			}
 		  	else
 			{
-				url = document.location+"";
-				return url.replace(results[0], results[1]+value);
+				url = url.replace(results[0], results[1]+value);
 			}
+			return url;
         }
     });
 })(jQuery);
