fbcde23b75189398586baa871e868e86d55720dc
hannes <hannes>
2009-05-27 07:48:04 +0000

    + clear <p style="margin: 0mm 0mm 0pt">
    Bug #267332
    Time:

--- automatweb/js/fckeditor/plugins/clear_styles/fckplugin.js
+++ automatweb/js/fckeditor/plugins/clear_styles/fckplugin.js
@@ -37,6 +37,7 @@ FCKClearStyles.CleanWordHTML = function(str)
 	str = str.replace(/<o:p>.*?<\/o:p>/g, "&nbsp;") ;
 	str = str.replace( /\s*mso-[^:]+:[^;"]+;?/gi, "" ) ;
 	str = str.replace( /\s*MARGIN: 0cm 0cm 0pt\s*;/gi, "" ) ;
+	str = str.replace( /\s*MARGIN: 0mm 0mm 0pt\s*;/gi, "" ) ;
 	str = str.replace( /\s*MARGIN: 0cm 0cm 0pt\s*"/gi, "\"" ) ;
 	str = str.replace( /\s*TEXT-INDENT: 0cm\s*;/gi, "" ) ;
 	str = str.replace( /\s*TEXT-INDENT: 0cm\s*"/gi, "\"" ) ;
